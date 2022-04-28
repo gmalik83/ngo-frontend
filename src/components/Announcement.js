fe87cb4a7d806f22db29b053776b656d746b77f4
gmalik83 <gaurav98malik@gmail.com>
2022-04-28 17:27:45 +0530

    Announcement  OK with Comments

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -3,11 +3,15 @@ import UserService from "./services/user.service";
 import "./Announcement.css";
 
 const Announcement = () => {
+  // Content Array for Fetching Announcements from Backend
   const [content, setContent] = useState([]);
+  // Loading State during API call or NO Response from Backend
   const [loading, setLoading] = useState(false);
+  // Message State for Any Errors
   const [message, setMessage] = useState("");
 
   useEffect(() => {
+    // GET Announcement Array from Backend
     UserService.getAnnouncement().then(
       (response) => {
         setLoading(true);
@@ -32,20 +36,25 @@ const Announcement = () => {
   }, []);
   return (
     <div>
-      <div className="panel panel-primary" style ={{margin:"25x 10px 25px 5px"}}>
+      <div
+        className="panel panel-primary"
+        style={{ margin: "25x 10px 25px 5px" }}
+      >
         <div className="panel-heading panel-heading-1 panel2">Announcement</div>
 
         <div className="panel-body panel1">
+          {/* Marquee Tag for UP Movement of Paragraph Array */}
           <marquee
             className="marquee"
             direction="up"
             scrolldelay="250"
             height="300"
           >
+            {/* If there is no Data from Backend */}
             {content.length === 0 ? (
               <div className="spinner-grow text-danger" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
+                <span className="sr-only">Loading...</span>
+              </div>
             ) : (
               <>
                 {content.map((item, i) => {
@@ -59,7 +68,7 @@ const Announcement = () => {
                 })}
               </>
             )}
-
+            {/* If message Exist (that means errors are present ) , then display this error in Box  */}
             {message && (
               <div className="form-group">
                 <div className="alert alert-danger" role="alert">
