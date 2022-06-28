547eb92dae60a5e854cfba673c596ec2cd9d80ac
gmalik83 <Gaurav98malik@gmail.com>
2022-06-29 01:21:17 +0530

    Index.js  Final Checked

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+// Main File to Render App
 ReactDOM.render(<App />, document.getElementById('root'));
  
